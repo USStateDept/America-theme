98530684f82953a44f0e485deae5919de85b2dd1
Nathan Kleekamp <nkleekamp@gmail.com>
2015-04-11 20:29:35 +0000

    basic climate css finished, moved specific styles from base theme to sub-directories, added general styles from sub-directories to base theme, adjusted location of taxonomies in base theme

--- front-page.php
+++ front-page.php
@@ -57,6 +57,11 @@ function america_add_home_body_class( $classes ) {
 }
 
 
+//* Reposition the post info
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+add_action( 'genesis_entry_header', 'genesis_post_info', 5 );
+
+
 //* Set default post info to just date
 add_filter( 'genesis_post_info', 'america_default_post_info_filter' );
 function america_default_post_info_filter($post_meta) {
