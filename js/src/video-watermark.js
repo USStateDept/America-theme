2632c0e4fd7c96a6a7833d8836583587791acd36
Nathan Kleekamp <nkleekamp@gmail.com>
2015-05-21 16:59:51 +0000

    moved scripts to footer, removed jquery ready

--- js/src/video-watermark.js
+++ js/src/video-watermark.js
@@ -1,16 +1,14 @@
 ;(function ( $, window, document, undefined ) {
 
-    $(document).ready(function() {
-        var $vidThumbs = $(
-          '.archive .content .format-video .entry-image,
-           .archive .content .category-video .entry-image,
-           .blog .content .format-video .entry-image,
-           .blog .content .category-video .entry-image'
-        );
+    var $vidThumbs = $(
+      '.archive .content .format-video .entry-image,
+       .archive .content .category-video .entry-image,
+       .blog .content .format-video .entry-image,
+       .blog .content .category-video .entry-image'
+    );
 
-        var $videoWatermark = $('<div/>').addClass('video-watermark');
+    var $videoWatermark = $('<div/>').addClass('video-watermark');
 
-        $vidThumbs.after($videoWatermark);
-    });
+    $vidThumbs.after($videoWatermark);
 
 })( jQuery, window, document );
--- sites/publications/js/src/init.js
+++ sites/publications/js/src/init.js
@@ -1,11 +1,9 @@
 ;(function ( $, window, document, undefined ) {
 
-    $(document).ready(function() {
-        // init responsive navigation
-        $('.genesis-nav-menu').responsiveMenu();
+    // init responsive navigation
+    $('.genesis-nav-menu').responsiveMenu();
 
-        // add dropdown arrow to wpml language switcher
-        $(".menu-item-language").addClass("menu-item-has-children");
-    });
+    // add dropdown arrow to wpml language switcher
+    $(".menu-item-language").addClass("menu-item-has-children");
     
 })( jQuery, window, document );
