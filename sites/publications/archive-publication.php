f496b6443727a113edbf8e74a2e3163644bcbf3e
Peter Kaizer <pdkaizer@gmail.com>
2016-01-19 15:28:03 -0500

    Edited styles for publication archive template

--- sites/publications/archive-publication.php
+++ sites/publications/archive-publication.php
@@ -1,93 +1,39 @@
 <?php
 /**
-* This file adds the publication type taxonomy archive template to the America.gov Theme.
-*
-* @author Office of Design, Bureau of International Information Programs
-* @package America.gov
-* @subpackage Customizations
-*/
+ * Template Name: Publications  Archives
+ */
 
-//* Force full width content layout
-add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
-
-//* Remove the breadcrumb navigation
-remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
-
-//* Remove the post info function
-remove_action( 'genesis_entry_header', 'genesis_post_info', 5 );
-
-//* Remove the post content
-remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
-
-//* Remove the post image
-remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
-
-//* Remove the post info function
-remove_action( 'genesis_entry_header', 'genesis_post_info', 5 );
-
-//* Remove the author box
-remove_action( 'genesis_after_entry', 'genesis_do_author_box', 8 );
-
-//* Remove Post Meta
-remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
-remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
-remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
-
-//* Add publication body class to the head
-add_filter( 'body_class', 'america_add_publication_body_class' );
-function america_add_publication_body_class( $classes ) {
-
-  $classes[] = 'america-publication';
-  return $classes;
-
+remove_action ('genesis_loop', 'genesis_do_loop'); // Remove the standard loop
+add_action( 'genesis_loop', 'custom_do_grid_loop' ); // Add custom loop
+function custom_do_grid_loop() {  
+  	
+	$args = array(
+		'post_type' => 'publications', // enter your custom post type
+		'posts_per_page'=> '12',  // overrides posts per page in theme settings
+	);
+	$loop = new WP_Query( $args );
+	if( $loop->have_posts() ):
+				
+		while( $loop->have_posts() ): $loop->the_post(); global $post;
+		echo '<div id="testimonials">';
+			echo '<div class="one-fourth first">';
+			echo '<div class="quote-obtuse"><div class="pic">'. get_the_post_thumbnail( $id, array(150,150) ).'</div></div>';
+			echo '<div style="margin-top:20px;line-height:20px;text-align:right;"><cite>'.genesis_get_custom_field( '_cd_client_name' ).'</cite><br />'.genesis_get_custom_field( '_cd_client_title' ).'</div>';
+			echo '</div>';	
+			echo '<div class="three-fourths" style="border-bottom:1px solid #DDD;">';
+			echo '<h3>' . get_the_title() . '</h3>';
+			echo '<blockquote><p>' . get_the_content() . '</p></blockquote>';	
+			echo '</div>';
+		echo '</div>';
+		
+		endwhile;
+		
+	endif;
+	
 }
-
-//* Add the featured image after post title
-add_action( 'genesis_entry_header', 'america_publication_grid', 5 );
-function america_publication_grid() {
-
-  if ( $image = genesis_get_image( 'format=url&size=publication' ) ) {
-    printf( '<div class="publication-featured-image"><a href="%s" rel="bookmark"><img src="%s" alt="%s" /></a></div>', get_permalink(), $image, the_title_attribute( 'echo=0' ) );
-  }
-}
-
-add_action( 'genesis_entry_header', 'america_publication_meta', 10);
-function america_publication_meta() {
-
-  // taxonomy pages (i.e. Books, Pamphlets, etc.) - display image, title and subjects
-  if ( is_tax() ) {
-    add_filter( 'genesis_post_meta', 'america_post_meta_category_tags_filter' );
-  }
-
-  // category pages (i.e. Democracy, Health, etc.) -  display image, title and publication type
-  if (is_category() ) {
-    add_filter( 'genesis_post_meta', 'america_post_meta_tags_type_filter' );
-  }
-
-  // search results page
-  if( is_search() ) {
-    add_filter( 'genesis_post_meta', 'america_post_meta_all_filter' );
-  }
-
-}
-
-function america_post_meta_all_filter($post_meta) {
-  $post_meta = '[post_categories before="Subject: "]';
-  $post_meta .= '[post_tags before="Tagged: "]';
-  $post_meta .= '[publication_type]';
-  return $post_meta;
-}
-
-function america_post_meta_category_tags_filter($post_meta) {
-  $post_meta = '[post_categories before="Subject: "]';
-  $post_meta .= '[post_tags before="Tagged: "]';
-  return $post_meta;
-}
-
-function america_post_meta_tags_type_filter($post_meta) {
-  $post_meta = '[post_tags before="Tagged: "]';
-  $post_meta .= '[publication_type]';
-  return $post_meta;
-}
-
-  genesis();
+	
+/** Remove Post Info */
+remove_action('genesis_before_post_content','genesis_post_info');
+remove_action('genesis_after_post_content','genesis_post_meta');
+ 
+genesis();
