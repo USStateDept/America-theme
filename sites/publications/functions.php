fe6a4502ef403f1e87f7799413774f5fb59b6664
Terri Regan <contact@terriregan.com>
2016-04-01 16:19:58 -0400

    Added GTM to publications site

--- sites/publications/functions.php
+++ sites/publications/functions.php
@@ -53,6 +53,23 @@ function amgov_pubs_search_filter($query) {
   }
 }
 
+
+
+//* Add Google Tag Manager
+add_action('genesis_before', 'amgov_pubs_gtm');
+function amgov_pubs_gtm() {
+  $html = '<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-T2866D"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({"gtm.start":
+new Date().getTime(),event:"gtm.js"});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";j.async=true;j.src=
+"//www.googletagmanager.com/gtm.js?id="+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,"script","dataLayer","GTM-T2866D");</script>
+<!-- End Google Tag Manager -->';
+  echo $html;
+}
+
 //* Remove Footer Credits
 remove_action( 'genesis_footer', 'genesis_do_footer' );
 add_action( 'genesis_footer', 'amgov_pubs_custom_footer' );
